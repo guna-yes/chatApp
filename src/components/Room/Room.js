f512223ffa3c7addd28a6b482a118e9b319cb676
guna-yes <i_settivaripalli.g@olcademy.in>
2021-05-22 19:27:27 +0530

    added(routes):to room comp

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
-export default function Room() {
-    return (
-        <div>
-            Room
-        </div>
-    )
-}
+import React, { useEffect, useState } from "react";
+import querystring from "query-string";
+const Room = ({ location }) => {
+  const [name, setname] = useState("");
+  const [room, setroom] = useState("");
+  useEffect(() => {
+    const { name, room } = querystring.parse(location.search);
+    setroom(room);
+    setname(name);
+    return () => {};
+  }, []);
+  return;
+  <div>
+      
+  </div>;
+};
+export default Room;
