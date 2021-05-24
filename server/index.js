4fb775b6251e44ab881e0e408435ac683fe24778
guna-yes <i_settivaripalli.g@olcademy.in>
2021-05-24 10:58:38 +0530

    added(server):userFunctions

--- server/index.js
+++ server/index.js
@@ -1,35 +1,54 @@
-const express=require('express');
-const http=require('http');
-const app=express();
-const server=http.createServer(app);
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
 const cors = require("cors");
-const PORT=8000;
-const router=require('./routers')
+const PORT = 8000;
+const router = require("./routers");
+
+const { addUser, removeUser, getUsersInRoom, getUser } = require("./users.js");
 
 const io = require("socket.io")(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    }
-  });
-app.use(router)
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+app.use(router);
 app.use(cors());
 
-io.on('connection',(socket) =>{
-    console.log('New connection')
+io.on("connection", (socket) => {
+  console.log("New connection");
 
-    socket.on('join',({name,room})=>{
-        console.log(name,room)
-    })
-    socket.on('disconnect',() =>{
-        console.log("Left")
-    })
-})
+  socket.on("join", ({ name, room }, callback) => {
+    // console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) {
+      return callback(error);
+    }
+    socket.emit("message", {
+      text: `${user.name},Welcome to the room ${user.room}`,
+    });
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name},has joined`,
+    });
+    socket.join(user.room);
+    callback();
+  });
 
+  socket.on('sendMessage',(message,callback) => {
+    const user =getUser(socket.id);
+    io.to(user.room).emit("message",{user:user.name,text:message})
+    callback();
 
+  })
 
+  socket.on("disconnec", () => {
+    console.log("Left");
+  });
+});
 
-
-server.listen(PORT,(req,res)=>{
-    console.log(`running on port ${PORT}`)
-})
+server.listen(PORT, (req, res) => {
+  console.log(`running on port ${PORT}`);
+});
--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,27 +1,25 @@
-
-
 import React, { useEffect, useState } from "react";
 import querystring from "query-string";
-import io from "socket.io-client"
-export default function Room({location}) {
+import io from "socket.io-client";
+export default function Room({ location }) {
+  const ENDPOINT = "http://localhost:8000/";
+  const [name, setname] = useState("");
+  const [room, setroom] = useState("");
+  useEffect(() => {
+    const { name, room } = querystring.parse(location.search);
+    setroom(room);
+    setname(name);
 
-const ENDPOINT="http://localhost:8000/"
-    const [name, setname] = useState("");
-    const [room, setroom] = useState("");
-    useEffect(() => {
-      const { name, room } = querystring.parse(location.search);
-      setroom(room);
-      setname(name);
-      
-     const  socket=io(ENDPOINT)
-     socket.emit('join',{name,room})
-      return () => {};
-    }, [ENDPOINT,location.search]);
-    return (
-        <div>
-        Room
-            
-        </div>
-    )
+    const socket = io(ENDPOINT);
+    socket.emit("join", { name, room });
+    return () => {
+      socket.emit("disconnec");
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+  return( 
+  <div>
+  Room
+  </div>
+  );
 }
-
